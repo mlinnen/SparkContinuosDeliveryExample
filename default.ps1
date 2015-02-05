298efe8a739f8a523e9f516114a4857f0e29df50
Mike Linnen <mlinnen@protosystem.net>
2015-02-05 00:49:04 -0500

    Assume curl is installed

--- default.ps1
+++ default.ps1
@@ -5,19 +5,11 @@ properties {
 
 task default -depends Deploy
 
-
-task Compile -depends Clean { 
-}
-
-task Clean { 
-  $cleanMessage
-}
-
 task Deploy { 
     $url = "https://api.spark.io/v1/devices/" + $deviceId + "?access_token=" + $token
     exec {
-      .\lib\curl\curl.exe -X PUT -F file=@src\helloworld.ino "$url" -k
-    }
+        curl -X PUT -F file=@src\helloworld.ino "$url" -k
+      }
 }
 
 task ? -Description "Helper to display task info" {
