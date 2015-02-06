af381bb173f4f1576d267fb3dcca2db78b6a041d
Mike Linnen <mlinnen@protosystem.net>
2015-02-05 23:08:21 -0500

    test

--- default.ps1
+++ default.ps1
@@ -11,7 +11,7 @@ task Clean {
 task Deploy { 
     $url = "https://api.spark.io/v1/devices/" + $deviceId + "?access_token=" + $token
     exec {
-        &.\lib\curl\curl.exe -X PUT -F file=@src\helloworld.ino "$url" -k
+        &.\lib\curl\curl.exe -X PUT -F file=@src\helloworld.ino "https://api.spark.io/v1/devices/" -k
       }
 } -depends Clean
 
