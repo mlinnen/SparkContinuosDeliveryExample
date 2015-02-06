f0260c1f473c35d9a5b56365f18c53e7410d4138
Mike Linnen <mlinnen@protosystem.net>
2015-02-05 22:04:17 -0500

    Created a clean task

--- default.ps1
+++ default.ps1
@@ -5,13 +5,16 @@ properties {
 
 task default -depends Deploy
 
+task Clean {
+    remove-item alias:curl
+}
+
 task Deploy { 
     $url = "https://api.spark.io/v1/devices/" + $deviceId + "?access_token=" + $token
     exec {
-        remove-item alias:curl
         curl -X PUT -F file=@src\helloworld.ino "$url" -k
       }
-}
+} -depends Clean
 
 task ? -Description "Helper to display task info" {
 	Write-Documentation
